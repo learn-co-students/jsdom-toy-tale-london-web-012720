230282ae50e22d67848b369b87e4dd97ada21194
github username <github email address>
2020-04-02 20:50:36 +0100

    Done.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Help Andy recover their toys and get the toys back in the toy collection.
 All of the toy data is stored in the `db.json` file. You'll want to access this
 data using a JSON server. In order to do this, run the following two commands:
 
-   * `npm install -g json-server`
+   * `npm install -g json-server` 
    * `json-server --watch db.json`
    
 This will create a server storing all of our lost toy data with restful routes
--- src/index.js
+++ src/index.js
@@ -1,15 +1,98 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  const baseUrl = 'http://localhost:3000'
+  const toysUrl = 'http://localhost:3000/toys'
+
+  const addToyForm = document.querySelector(".add-toy-form")
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-    } else {
-      toyForm.style.display = "none";
-    }
+      // hide & seek with the form
+      addToy = !addToy;
+      if (addToy) {
+        toyForm.style.display = "block";
+      } else {
+        toyForm.style.display = "none";
+      }
   });
-});
+ 
+  function getToys() {
+    fetch(toysUrl) 
+    .then( response => response.json() )
+    .then( toysArray => renderToys(toysArray) )
+  }
+  getToys();
+
+  function renderToys(toysArray) {
+    toysArray.forEach(toy => renderSingleToy(toy))
+  }
+      
+  function renderSingleToy(toy) {
+    const toyCollection = document.querySelector("#toy-collection")
+    const toyCard = document.createElement('div');
+    const toyName = document.createElement("h2");
+    const toyImg = document.createElement("img");
+    const toyLikes = document.createElement('p');
+    const likeButton = document.createElement('button');
+
+    toyCard.className = "card";
+    toyName.innerText = toy.name;
+    toyImg.className = "toy-avatar";
+    toyImg.src = toy.image;
+    toyLikes.innerText = `${toy.likes} Likes`;
+    likeButton.className = "like-btn";
+    likeButton.innerText = "Like <3";
+
+    likeButton.addEventListener('click', () => {
+      increaseLikes(toy, toyLikes)
+    })
+    toyCard.append(toyName, toyImg, toyLikes, likeButton)
+    toyCollection.append(toyCard)
+  }
+
+  addToyForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newToyName = e.currentTarget.name.value;
+    const toyImageURL = e.currentTarget.image.value;
+    addNewToy(newToyName, toyImageURL)
+    e.target.reset();
+  })
+
+  function addNewToy(name, image) {
+    fetch(toysUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        image: image,
+        likes: 0,
+      })
+
+    })
+    .then(res => res.json())
+    .then(newToy => renderSingleToy(newToy));
+  }
+  
+  function increaseLikes(toy, toyLikes) {
+    toy.likes++
+    toyLikes.innerText = `${toy.likes} Likes`
+    fetch(toysUrl + `/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        likes: toy.likes,
+      }),
+    })
+  }
+
+
+})
